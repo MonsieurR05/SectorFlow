13200b3a5bdb2db54cfdbef39746d7b7a3452d16
Rahul Bagga <MonsieurR9@proton.me>
2026-05-04 12:45:40 +0100

    chore [backend]: initialise Rust project

---
+++ backend/target/debug/backend.d
@@ -0,0 +1,1 @@
+C:\Users\Monsi\Downloads\SectorFlow\backend\target\debug\backend.exe: C:\Users\Monsi\Downloads\SectorFlow\backend\src\main.rs
---
+++ backend/target/debug/deps/backend.d
@@ -0,0 +1,5 @@
+C:\Users\Monsi\Downloads\SectorFlow\backend\target\debug\deps\backend.d: src\main.rs
+
+C:\Users\Monsi\Downloads\SectorFlow\backend\target\debug\deps\backend.exe: src\main.rs
+
+src\main.rs:
